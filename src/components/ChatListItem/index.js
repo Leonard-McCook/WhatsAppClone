963fc33d7bf4fc26f5d82ead23249e26c6f713c9
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-10-25 02:08:15 -0400

    Add styles tag to HTML elements

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,9 +1,29 @@
-import {Text} from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 const ChatListItem = () => {
     return (
-        <Text>ChatListItem</Text>
-    )
-}
+        <View style={styles.container}>
+            <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg'}} 
+            style={styles.image} 
+            />
+            <View style={styles.content}>
+              <View style={styles.row}>
+                <Text style={styles.name}>Lukas</Text>
+                <Text style={styles.subTitle}>8:30</Text>
+              </View>
+              
+              
+            <Text style={styles.subTitle}>Hello there</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    image: {
+        width: 60,
+        height: 60,
+    }
+})
 
 export default ChatListItem;
