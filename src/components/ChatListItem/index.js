d445d26449eaab0e170378ae4fdf0129956b341e
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-10-27 23:56:16 -0400

    Add flexDirection to styling

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -3,15 +3,16 @@ import { Text, View, Image, StyleSheet } from 'react-native';
 const ChatListItem = () => {
     return (
         <View style={styles.container}>
-            <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg'}} 
+            <Image 
+            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg'}} 
             style={styles.image} 
             />
+
             <View style={styles.content}>
               <View style={styles.row}>
                 <Text style={styles.name}>Lukas</Text>
                 <Text style={styles.subTitle}>8:30</Text>
               </View>
-              
               
             <Text style={styles.subTitle}>Hello there</Text>
             </View>
@@ -20,16 +21,33 @@ const ChatListItem = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        marginVertical: 5,
+        height: 70
+    },
+
     image: {
         width: 60,
         height: 60,
         borderRadius: 30,
+        marginRight: 10,
     },
-    container: {},
-    row: {},
-    name: {},
-    subTitle: {},
+    content: {
+        flex: 1,
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 5,
+    },
+    name: {
+        flex: 1,
+        fontWeight: 'bold',
+    },
+    subTitle: {
+        color: 'gray'
+    },
 });
 
 export default ChatListItem;
