e0f7b6bbce8a0ea5cc5ce3aebd7aeadf098278db
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-10-30 19:29:54 -0400

    Add dummy date for the chat list

--- src/screens/ChatsScreen.js
+++ src/screens/ChatsScreen.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import chats from '../../assets/data/chats.json'
+import ChatListItem from '../components/ChatListItem'
 
 const ChatsScreen = () => {
   return (
-    <View>
-      <Text>ChatsScreen</Text>
-    </View>
-  )
-}
+    <FlatList 
+      data={chats} 
+      renderItem={({ item }) => <ChatListItem chat={item} />} 
+    />
+  );
+};
 
 export default ChatsScreen
