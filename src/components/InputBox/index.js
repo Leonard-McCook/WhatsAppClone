3b1597612ac6e88f48a528b04a8f6da8a0814157
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-04 00:59:24 -0400

    Add OnSend function warning to InputBox

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -3,11 +3,18 @@ import { AntDesign } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const InputBox = () => {
+
+   // state data
+
+  const onSend =  () => {
+    console.warn('Sending a new message');
+  };
+  
   return (
     <View style={styles.container}>
       <AntDesign name="plus" size={24} color="royalblue" />
-      <TextInput style={styles.input} />
-      <MaterialIcons style={styles.send} name="send" size={24} color="white" />
+      <TextInput style={styles.input}  placeholder="Type your message..."/>
+      <MaterialIcons onPress={onSend} style={styles.send} name="send" size={24} color="white" />
     </View>
   );
 };
