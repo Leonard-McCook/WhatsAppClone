41ee2d22c2d05ff6e425062d5593a1511ae24225
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-04 00:38:07 -0400

    Import KeyboardAvoidingView

--- src/screens/IndividualChatScreen.js
+++ src/screens/IndividualChatScreen.js
@@ -1,4 +1,4 @@
-import { Text, ImageBackground, StyleSheet, FlatList } from "react-native";
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
 import Message from "../components/Message";
 import InputBox from '../components/InputBox';
 
@@ -7,15 +7,19 @@ import messages from '../../assets/data/messages.json';
 
 const IndividualChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList 
-        data={messages} 
-        renderItem={({ item }) => <Message message={item} />} 
-        style={styles.list}
-        inverted
-      />
-      <InputBox />
-    </ImageBackground>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    style={styles.bg}>
+        <ImageBackground source={bg} style={styles.bg}>
+          <FlatList 
+            data={messages} 
+            renderItem={({ item }) => <Message message={item} />} 
+            style={styles.list}
+            inverted
+          />
+          <InputBox />
+        </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
