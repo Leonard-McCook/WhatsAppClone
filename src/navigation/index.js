c94a5b1f0aeb5d908586d6fdb3509fd5636413c8
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-07 14:14:29 -0500

    Implement stack nagvigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import IndividualChatScreen from '../screens/IndividualChatScreen';
+import ChatsScreen from '../screens/ChatsScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <IndividualChatScreen />
+      <Stack.Navigator>
+         <Stack.Screen name='Chats' component={ChatsScreen} />
+         <Stack.Screen name='Chat' component={IndividualChatScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
